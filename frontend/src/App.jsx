f9b16a9c1a77d66eba49c071c3513589d908f456
duttabikram <136303237+duttabikram@users.noreply.github.com>
2025-12-16 15:54:19 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef, useState } from "react";
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000");
+const socket = io("http://videobackend-llpa.onrender.com");
 
 function App() {
   const localVideoRef = useRef(null);
