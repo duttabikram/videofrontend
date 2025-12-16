13ae86845a17d0e377004af75a905cb0c0405a7d
duttabikram <136303237+duttabikram@users.noreply.github.com>
2025-12-16 22:19:51 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -173,49 +173,8 @@ function leaveCall() {
       >
         <h2>Google Meet Clone (1-to-1)</h2>
 
-       <div
-  style={{
-    width: "100%",
-    maxWidth: "600px",
-    aspectRatio: "16 / 9",
-    background: "#000",
-    marginBottom: "10px",
-  }}
->
-  <video
-    ref={localVideoRef}
-    autoPlay
-    muted
-    playsInline
-    style={{
-      width: "100%",
-      height: "100%",
-      objectFit: "cover",
-    }}
-  />
-</div>
-
-
-        <div
-  style={{
-    width: "100%",
-    maxWidth: "600px",
-    aspectRatio: "16 / 9",
-    background: "#000",
-  }}
->
-  <video
-    ref={remoteVideoRef}
-    autoPlay
-    playsInline
-    style={{
-      width: "100%",
-      height: "100%",
-      objectFit: "cover",
-    }}
-  />
-</div>
-
+       <video ref={localVideoRef} autoPlay  playsInline style={{ width: "300px", margin: "10px" }} /> 
+       <video ref={remoteVideoRef} autoPlay playsInline style={{ width: "300px", margin: "10px" }} />
 
         <div style={{ marginTop: "10px" }}>
           <button onClick={toggleCamera}>
